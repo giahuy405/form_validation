41f91006bdf96119e7d6a1addca8e58517519991
giahuy405 <pgiahuy562@gmail.com>
2022-09-04 21:16:27 +0700

    up

--- .history/validation_20220904211600.js
+++ .history/validation_20220904211600.js
@@ -19,16 +19,3 @@ function dangnhap(){
     a_DangKi.style.backgroundColor="#808080" ;
     a_DangNhap.style.backgroundColor="#282a37" ;
 }
-function kiemtrabatbuoc(){
-    var tendangnhap=document.getElementById("tendangnhap");
-    var matkhau=document.getElementById("matkhau");
-    var email=document.getElementById("email");
-    var tuoi=document.getElementById("tuoi");
-    // var 
-    // // if(form1.tendangnhap.value==""||form1.matkhau.value==""||form1.email.value==""||form1.tuoi.value==""){
-
-    // // }
-    // if(form1.tendangnhap.value==""){
-
-    // }
-}
